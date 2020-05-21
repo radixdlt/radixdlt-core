11221190a33d1763e5d748c44be23439392d4ded
Florian Cäsar <florian@radixdlt.com>
2020-05-21 18:35:46 +0200

    Revert "Disable fee check"
    This reverts commit ff8207f1d34e3450cc07354e8ce7f4a665e0e4b5.

--- radixdlt/src/main/java/com/radixdlt/middleware2/MiddlewareModule.java
+++ radixdlt/src/main/java/com/radixdlt/middleware2/MiddlewareModule.java
@@ -105,7 +105,7 @@ public class MiddlewareModule extends AbstractModule {
 			virtualStoreLayer,
 			engineStore
 		);
-//		radixEngine.addCMSuccessHook(ledgerAtomChecker); TODO Hack: Disabled fee check for chess hackathon
+		radixEngine.addCMSuccessHook(ledgerAtomChecker);
 		radixEngine.addAtomEventListener(new EngineAtomEventListener(serialization));
 
 		return radixEngine;
